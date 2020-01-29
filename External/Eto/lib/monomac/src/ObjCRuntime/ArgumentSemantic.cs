0978bbd62100f844e83d445aff7e9a69840989fc
Angry Wasp <angrywasp@live.com>
2020-01-29 14:18:28 +1100

    Eto nuget packages are very outdated. Use the master branch code directly

---
+++ External/Eto/lib/monomac/src/ObjCRuntime/ArgumentSemantic.cs
@@ -0,0 +1,10 @@
+namespace MonoMac.ObjCRuntime {
+	public enum ArgumentSemantic {
+		None = -1,
+		Assign = 0,
+		Copy = 1,
+		Retain = 2,
+        Strong = 3, // not implemented
+        Weak = 4 // not implemented
+	}
+}
