4723170e28f50a3f9d65e0f7502edc210c2703ca
Angry Wasp <angrywasp@live.com>
2020-09-11 20:20:16 +1000

    More build script and csproj changes

--- builder/build.bat
+++ builder/build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 dotnet publish ^
-/p:GenerateFullPaths=true /p:Configuration=Release /p:TrimUnusedDependencies=true ^
+/p:Publish=true /p:GenerateFullPaths=true /p:Configuration=Release /p:TrimUnusedDependencies=true ^
 /p:OutputPath=%~dp0..\Bin\Release\win-x64\ %~dp0..\Src\Nerva.Toolkit\Nerva.Toolkit.Windows.csproj
