34b44389011abb058b0e67760ebf8fa376e55a5d
Angry Wasp <angrywasp@live.com>
2021-01-25 16:20:12 +1100

    Mac build fixes

--- builder/debug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-dotnet build ^
-/p:GenerateFullPaths=true /p:Configuration=Debug /p:TrimUnusedDependencies=true ^
-/p:OutputPath=%~dp0..\Bin\Debug\win-x64\ %~dp0..\Src\Nerva.Toolkit\Nerva.Toolkit.Windows.csproj
