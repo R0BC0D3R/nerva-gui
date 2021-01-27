467e41ca14e73dc73184b21c5f309145c28f4da3
Angry Wasp <angrywasp@live.com>
2021-01-27 22:42:07 +1100

    Show error message on failure to start mining

--- Src/Nerva.Toolkit/MainForm.cs
+++ Src/Nerva.Toolkit/MainForm.cs
@@ -277,7 +277,13 @@ namespace Nerva.Toolkit
                 }
                 else
                     if (DaemonRpc.StartMining())
-                    Log.Instance.Write($"Mining started for @ {Conversions.WalletAddressShortForm(Configuration.Instance.Daemon.MiningAddress)} on {Configuration.Instance.Daemon.MiningThreads} threads");
+                        Log.Instance.Write($"Mining started for @ {Conversions.WalletAddressShortForm(Configuration.Instance.Daemon.MiningAddress)} on {Configuration.Instance.Daemon.MiningThreads} threads");
+                    else
+                        Application.Instance.AsyncInvoke( () =>
+                        {
+                            MessageBox.Show(Application.Instance.MainForm, $"Failed to start mining.\r\nMake sure you are synced and check your mining address", "Toggle Miner",
+                            MessageBoxButtons.OK, MessageBoxType.Error, MessageBoxDefaultButton.OK);
+                        });
             }, null);
         }
 
